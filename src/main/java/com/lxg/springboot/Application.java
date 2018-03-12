243600dd77de16927a0cd182e350b869774071e4
mhs1314 <471002385@qq.com>
2018-03-12 18:18:22 +0800

    参考资料

--- src/main/java/com/lxg/springboot/Application.java
+++ src/main/java/com/lxg/springboot/Application.java
@@ -1,7 +1,7 @@
 package com.lxg.springboot;
 
 import org.springframework.boot.SpringApplication;
-
+//参考博客：http://blog.csdn.net/u013783079/article/details/75307785
 /**
  * Created by lxg
  * on 2017/2/18.
